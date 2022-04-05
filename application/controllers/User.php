e3a9da499d2b1a59e6a375a45269ff8b060f7d95
murisdotcom <muhrizalsidik@gmail.com>
2022-04-05 11:09:30 +0700

    Memisahkan header & footer serta memperbaiki halaman user/index

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,14 +1,16 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
+ <?php
+	defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
-{
-
-	public function index()
+	class User extends CI_Controller
 	{
-		$judul['title'] = 'SIMGLOW';
-		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-		$this->load->view('user/index', $judul);
+		public function index()
+		{
+			$data['title'] = 'User | MS GLOW';
+			$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('user/index', $data);
+			$this->load->view('templates/footer', $data);
+		}
 	}
-}
