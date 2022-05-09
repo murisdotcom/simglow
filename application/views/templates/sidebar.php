2c5e5bb630f7453828340aaa06cebf36d8049cb2
murisdotcom <muhrizalsidik@gmail.com>
2022-05-10 00:04:49 +0700

    Mengubah tampilan sidebar agar ada slide ke bawahnya

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -61,9 +61,21 @@
 
 						<ul class="nav side-menu">
 							<li>
-								<a href="<?= base_url($sm['url']); ?>">
-									<i class="<?= $sm['icon']; ?>"></i> <?= $sm['title']; ?>
-								</a>
+								<?php if ($sm['title'] == 'Produk') : ?>
+									<a>
+										<i class="<?= $sm['icon']; ?>"></i> <?= $sm['title']; ?><span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="tables.html">Kategori Produk</a></li>
+										<li><a href="tables_dynamic.html">Satuan Produk</a></li>
+										<li><a href="<?= base_url($sm['url']); ?>">Produk</a></li>
+										<li><a href="<?= base_url($sm['url']); ?>">Stok Masuk</a></li>
+										<li><a href="<?= base_url($sm['url']); ?>">Stok Keluar</a></li>
+									</ul>
+								<?php else : ?>
+									<a href="<?= base_url($sm['url']); ?>">
+										<i class="<?= $sm['icon']; ?>"></i> <?= $sm['title']; ?>
+									</a>
+								<?php endif; ?>
 							</li>
 							<!-- <li>
 								<a><i class="fa fa-fw fa-users"></i> Pelanggan</a>
